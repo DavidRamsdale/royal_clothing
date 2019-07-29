c79768087c1aa8f02691198e9aec578f69f4e231
David Ramsdale <davidramsdale1991@gmail.com>
2019-07-30 00:48:22 +1000

    Add Sign in form

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component'
+import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />        
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUp} />        
       </Switch>
     </div>
   );
