7e4fae0a1e43ec0d9bbe7d8dc6ad2e1c6b54673d
David Ramsdale <davidramsdale1991@gmail.com>
2019-07-30 00:28:56 +1000

    Add header component

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -9,8 +9,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
             style={{
                 backgroundImage: `url(${imageUrl})`
             }}
-        >
-        </div>
+        />
         <div className='collection-footer'>
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
